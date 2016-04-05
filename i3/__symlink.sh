923f583b332d9a55f1020cd5a83e6911dc4c772f
Sasa Savic <sasa.savic@protonmail.com>
2016-04-05 23:14:19 +0200

    [i3] Add status bar script

--- i3/__symlink.sh
+++ i3/__symlink.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 i3config="$DIR/config"
+status_script="$DIR/status"
 ln -s $i3config ~/.config/i3/config
+ln -s $status_script ~/.status
