33cfc1c49def129965f5648f835c998da7f9b3e0
Sasa Savic <sasa.savic@protonmail.com>
2016-04-29 10:42:04 +0200

    [steam] Change folder for cross distro use

--- steam/__symlink.sh
+++ steam/__symlink.sh
@@ -4,7 +4,7 @@ csgo="$DIR/games/csgo"
 csgo_autoexec="$csgo/autoexec.cfg"
 csgo_dm="$csgo/dm.cfg"
 csgo_surf="$csgo/surf.cfg"
-local_cfg_dir=~/.local/share/Steam/steamapps/common/Counter-Strike\ Global\ Offensive/csgo/cfg/
+local_cfg_dir=~/.steam/steam/steamapps/common/Counter-Strike\ Global\ Offensive/csgo/cfg/
 ln -sv $csgo_autoexec "$local_cfg_dir"
 ln -sv $csgo_dm "$local_cfg_dir"
 ln -sv $csgo_surf "$local_cfg_dir"
