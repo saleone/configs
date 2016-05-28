31a1a1edf460d59e6671907fb21849a1f7ffc073
Sasa Savic <sasa.savic@protonmail.com>
2016-05-28 19:17:30 +0200

    [vim] Install Vim-plug before linking .vimrc file

--- vim/__symlink.sh
+++ vim/__symlink.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 VIMRC="$DIR/vimrc"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s $VIMRC ~/.vimrc
