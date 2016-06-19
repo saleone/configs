03e1a57490f0e2296fc3a1365ef917b1438a4627
Saša Savić <sasa.savic@protonmail.com>
2016-06-19 22:43:00 +0200

    [i3] Create directory before linking files.

--- i3/__symlink.sh
+++ i3/__symlink.sh
@@ -2,5 +2,6 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 i3config="$DIR/config"
 status_script="$DIR/status"
+mkdir ~/.config/i3/
 ln -s $i3config ~/.config/i3/config
 ln -s $status_script ~/.status
