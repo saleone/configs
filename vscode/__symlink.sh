f25b0a6a29acc6228a9bab331a18c83711d8288b
Saša Savić <sasa.savic@protonmail.com>
2016-08-21 22:18:15 +0200

    Add Visual Studio Code settings and keybinds

---
+++ vscode/__symlink.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+settings="$DIR/settings.json"
+
+if [ -f ~/.config/Code/User/settings.json ]; then
+    rm ~/.config/Code/User/settings.json
+fi
+
+ln -s $settings ~/.config/Code/User/settings.json
+
+keybindings="$DIR/keybindings.json"
+
+if [ -f ~/.config/Code/User/keybindings.json ]; then
+    rm ~/.config/Code/User/keybindings.json
+fi
+
+ln -s $keybindings ~/.config/Code/User/keybindings.json
