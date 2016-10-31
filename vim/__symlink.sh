f9f105549d580e43e7d5db95720f74e27f523969
Saša Savić <savic-sasa@outlook.com>
2016-10-31 20:35:10 +0100

    Check if vimrc already exists and delete it

--- vim/__symlink.sh
+++ vim/__symlink.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 VIMRC="$DIR/vimrc"
+if [ -f ~/.vimrc ]; then
+    rm ~/.vimrc
+fi
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s $VIMRC ~/.vimrc
