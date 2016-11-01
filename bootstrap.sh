ef96332060a35d58563597043c5749aefe7e33ae
Saša Savić <savic-sasa@outlook.com>
2016-11-01 08:57:48 +0100

    [boostrap] Remove commented lines and force install dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,15 +6,15 @@ DATA=$HOME/Data
 cd $HOME
 
 echo " > Update system"
-#sudo apt update
-#sudo apt upgrade -y
+sudo apt update
+sudo apt upgrade -y
 
 echo " > Mount the data drive to: $HOME/Data"
-#mkdir -p $DATA
-#sudo mount /dev/sda2 $DATA
+mkdir -p $DATA
+sudo mount /dev/sda2 $DATA
 
 echo " > Make Dev folder: $DATA/Dev"
-#mkdir -p $DATA/Dev
+mkdir -p $DATA/Dev
 
 echo " > Create links to common folders"
 echo "    - $DATA/Dev -> $HOME/Dev"
@@ -37,7 +37,7 @@ if [ ! -L $HOME/GDrive ]; then
 fi
 
 echo " > Installing Git"
-sudo apt install git
+sudo apt install -y git
 
 echo " > Cloning configs from https://github.com/saleone/configs.git"
 git clone https://github.com/saleone/configs.git $DATA/Dev/configs
@@ -46,10 +46,10 @@ echo " > Set up Git"
 bash $DATA/Dev/configs/git/__symlink.sh
 
 echo " > Install Vim with Gtk3"
-sudo apt install vim-gtk3
+sudo apt install -y vim-gtk3
 
 echo " > Install Curl"
-sudo apt install curl
+sudo apt install -y curl
 
 echo " > Set up Vim"
 bash $DATA/Dev/configs/vim/__symlink.sh
@@ -65,10 +65,10 @@ echo " > Set up Urxvt"
 bash $DATA/Dev/configs/urxvt/__symlink.sh
 
 #echo " > Install Visual Studio Code"
-#wget https://go.microsoft.com/fwlink/?LinkID=760868 -O vscode.deb
-#sudo dpkg -i vscode.deb
-#rm -f vscode.deb
-echo " > Get VS Code from: https://go.microsoft.com/fwlink/?LinkID=760868"
+wget https://go.microsoft.com/fwlink/?LinkID=760868 -O vscode.deb
+sudo dpkg -i vscode.deb
+rm -f vscode.deb
+sudo apt install -f
 
 echo " > Set up Visual Studio Code"
 bash $DATA/Dev/configs/vscode/__symlink.sh
@@ -80,6 +80,7 @@ echo " > Install Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
 sudo dpkg -i chrome.deb
 rm -f chrome.deb
+sudo apt install -f
 
 echo " > Install Node.js"
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
