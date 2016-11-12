7effe269671a73cc29445283ec9a1018dec088b5
Saša Savić <savic-sasa@outlook.com>
2016-11-12 14:34:22 +0100

    [urxvt] Remove old links before adding new

--- Xresources/__symlink.sh
+++ Xresources/__symlink.sh
@@ -2,8 +2,15 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 xresources="$DIR/Xresources"
 xkrclipboard="$DIR/xkr-clipboard.pl"
-if [ ! -d /usr/lib/urxvt/perl/ ]; then 
+if [ ! -d /usr/lib/urxvt/perl/ ]; then
     sudo mkdir -pv /usr/lib/urxvt/perl/
 fi
+if [ -f /usr/lib/urxvt/perl/xkr-clipboard ]; then
+    sudo rm -f /usr/lib/urxvt/perl/xkr-clipboard
+fi
 sudo ln -s $xkrclipboard /usr/lib/urxvt/perl/xkr-clipboard
+
+if [ -f ~/.Xresources ]; then
+    rm -f ~/.Xresources
+fi
 ln -s $xresources ~/.Xresources
