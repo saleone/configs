abc0ef19281560b546d7617c904d0c45225b05ae
Saša Savić <sasa@savic.pw>
2017-11-05 12:30:42 +0100

    Strip down the setup script for ubuntu

--- setups/ubuntu.sh
+++ setups/ubuntu.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 HOME=/home/saleone
 cd $HOME
-echo ">>> Ubuntu bootstrap script <<<"
-echo "! Please note that you may lose data when running this script !"
-echo "Do you want to proceed? (yes/NO)"
-read START_PROMPT
-if [ ! $START_PROMPT ==  "yes" ]; then exit 0; fi
 
 echo " > Update system"
 sudo apt update
@@ -38,10 +33,6 @@ echo " > Set up Mercurial"
 sudo apt install mercurial -y
 bash $HOME/Dev/configs/mercurial/__symlink.sh
 
-echo "Creating SSH key"
-echo " > Create SSH key"
-ssh-keygen -t rsa -b 4096 -C "$(git config --global user.email)"
-
 echo " > Install Curl"
 sudo apt install -y curl
 
@@ -57,7 +48,7 @@ echo " > Install and setup Visual Studio Code"
 wget https://go.microsoft.com/fwlink/?LinkID=760868 -O vscode.deb
 sudo dpkg -i vscode.deb
 rm -f vscode.deb
-sudo apt install -f
+sudo apt install -f -y
 bash $HOME/Dev/configs/vscode/__symlink.sh
 bash $HOME/Dev/configs/vscode/install_extensions.sh
 
@@ -68,45 +59,18 @@ echo " > Install Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
 sudo dpkg -i chrome.deb
 rm -f chrome.deb
-sudo apt install -f
+sudo apt install -f -y
 
 echo " > Install Node.js"
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt install -y nodejs
 sudo npm install --global npm
 
-echo " > Install bower"
-sudo npm install -g bower
-
 echo " > Install gulp"
 sudo npm install -g gulp-cli
 
 echo " > Install build essentials"
 sudo apt install -y build-essential
-
-echo " > Install Htop"
-sudo apt install -y htop
-
-echo " > Install redshift"
-sudo apt install redshift -y
-bash $HOME/Dev/configs/redshift/__symlink.sh
-
-echo "Would you like to install Steam? (yes/No)"
-echo " > Install Steam"
-sudo apt install steam -y
-
-echo " > Install Fira Fonts"
-wget https://github.com/mozilla/Fira/archive/4.202.zip -O fira.zip
-unzip fira.zip
-if [ ! -d $HOME/.fonts ]; then
-    mkdir -p $HOME/.fonts
-fi
-cp Fira-4.202/ttf/*.ttf $HOME/.fonts/
-rm -f fira.zip
-rm -fr Fira-4.202/
-
-echo " > Install Docker"
-curl -sSL https://get.docker.com/ | sh
 
 echo " > Install pip"
 sudo apt install python-pip python3-pip -y
@@ -126,13 +90,4 @@ sudo apt install editorconfig -y
 
 echo " > Install 7zip support"
 sudo apt install p7zip-full
-
-echo " > Install plank"
-sudo apt install plank -y
-
-echo " > Install Paper cursor and icon themes"
-sudo add-apt-repository ppa:snwh/pulp -y
-sudo apt update
-sudo apt install paper-cursor-theme -y
-sudo apt install paper-icon-theme -y
 
