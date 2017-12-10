956f92cbc5a55affa6a42e0564e54ea0faf3e474
Saša Savić <sasa@savic.pw>
2017-12-10 18:42:44 +0100

    [xfce] Use Nord for terminal theme

--- xfce4/__symlink.sh
+++ xfce4/__symlink.sh
@@ -3,3 +3,4 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 xfce4_config="$DIR/xfce4"
 rm -fr ~/.config/xfce4
 ln -s $xfce4_config ~/.config/xfce4
+ln -s $DIR/themes/nord.theme ~/.local/share/xfce4/terminal/colorschemes/nord.theme
