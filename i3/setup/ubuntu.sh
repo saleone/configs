4dedde4f0ebea37dcdf6348009970c34b313cbe5
Saša Savić <sasa@savic.pw>
2017-12-10 18:48:43 +0100

    [i3] Add ubuntu setup script

---
+++ i3/setup/ubuntu.sh
@@ -0,0 +1,12 @@
+# Download Arc theme package for <= 16.04
+# Download latest Fira Font Family
+
+# Run these packages and set correct theme
+sudo apt install lxappearance gtk-chtheme qtconfig-qt4
+
+# When started for the first time icons should be adjusted in settings
+sudo apt install volumeicon-alsa
+
+# Background should be saved in the home folder of the user as .wallpaper.{jpg, png}
+sudo apt install feh
+
