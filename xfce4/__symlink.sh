c3bc9c50d86a740f90cd0149cca939d455f758dc
Saša Savić <sasa@savic.pw>
2017-12-24 17:45:36 +0100

    [xfce] Create folder for terminal theme

--- xfce4/__symlink.sh
+++ xfce4/__symlink.sh
@@ -3,4 +3,5 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 xfce4_config="$DIR/xfce4"
 rm -fr ~/.config/xfce4
 ln -s $xfce4_config ~/.config/xfce4
+mkdir -p ~/.local/share/xfce4/terminal/colorschemes/
 ln -s $DIR/themes/nord.theme ~/.local/share/xfce4/terminal/colorschemes/nord.theme
