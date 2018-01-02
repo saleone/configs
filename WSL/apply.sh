da5dd43f70d0a65b65a802a07127e3c5aca7eaca
Saša Savić <sasa@savic.pw>
2018-01-02 14:03:49 +0100

    [wsl] Make sure that WSL configs can be applied

--- WSL/apply.sh
+++ WSL/apply.sh
@@ -2,20 +2,31 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0])}")" && pwd)"
 
 # i3
+rm -fr ~/.config/i3_wsl_old
+rm -fr ~/.config/.stastus_old
+mv ~/.config/i3 ~/.config/i3_wsl_old
+mv ~/.status ~/.status_old
 mkdir -p ~/.config/i3
 ln -s "$DIR/i3/config" ~/.config/i3/config
 ln -s "$DIR/i3/status" ~/.status
 chmod +x ~/.status
 
 # Git
+rm -fr ~/.gitconfig_wsl_old
+mv ~/.gitconfig ~/.gitconfig_wsl_old
 ln -s "$DIR/git/gitconfig" ~/.gitconfig
 
 # Bash
+rm -fr ~/.bashrc_wsl_old
 mv ~/.bashrc ~/.bashrc_wsl_old
 ln -s "$DIR/bash/bashrc" ~/.bashrc
 source ~/.bashrc
 
 # Terminal
-rm -fr ~/.config/xfce4
+rm -fr ~/.config/xfc4_wsl_old
+mv ~/.config/xfce4 ~/.config/xfce4_wsl_old
 mkdir -p ~/.config/xfce4/terminal
 ln -s "$DIR/xfce4/terminalrc" ~/.config/xfce4/terminal/terminalrc
+
+# Vim
+bash vim/__symlink.sh
