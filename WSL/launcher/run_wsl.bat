4e615e02aa2f4423eae01c2ffc0e614562d01f1d
Saša Savić <sasa@savic.pw>
2018-01-06 10:54:10 +0100

    [wsl] Allow i3 to start from start menu

--- WSL/launcher/run_wsl.bat
+++ WSL/launcher/run_wsl.bat
@@ -1,4 +1,4 @@
-.\seemless.xlaunch
-ping 127.0.0.1 -n 2 > nul
-
+:: Adding the location of the script to PATH allows us to run
+:: WSL from Run or searching it in Start Menu
+%~dp0\seemless.xlaunch
 bash -c "bash --rcfile <(echo '. ~/.bashrc; i3')"
