bc3ddf64ccc8b6db8f791780ffceaef3df4cd060
Saša Savić <savic-sasa@outlook.com>
2018-11-05 19:18:29 +0100

    Add fedora minimal install script

--- setups/fedora.sh
+++ setups/fedora.sh
@@ -6,24 +6,55 @@ sudo dnf update -y
 
 # Add RPM Fusion repository
 su -c 'dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y'
+dnf copr enable nforro/i3-gaps -y
+dnf copr enable jstanek/ports -y
 
-# Remove excess folders
-if [ -d $HOME/Public ]; then
-    rmdir $HOME/Public
-fi
+dnf install \
+    @base-x \
+    git \
+    @development-tools \
+    i3-gaps \
+    i3lock \
+    ranger \
+    NetworkManager-wifi \
+    firefox \
+    pulseaudio \
+    pulseaudio-utils \
+    vlc \
+    podman \
+    tmux \
+    vim-enchanced \
+    vim-X11 \
+    ncpamixer \
+    rsync \
+    podman \
+    vagrant \
+    VirtualBox \
+    ImageMagick \
+    feh \
+    wget \
+    curl \
+    bash-completion \
+    arandr \
+    compton \
+    redshift \
+    -y
 
-if [ -d $HOME/Templates ]; then
-    rmdir $HOME/Templates
-fi
-
-# Make Dev folder and clone the configuration
 CONFIGS="$HOME/Dev/configs"
 if [ ! -d $HOME/Dev ]; then
     mkdir $HOME/Dev
     git clone https://github.com/saleone/configs $CONFIGS
 fi
 
-# Link the configs
+echo "exec i3" >> ~/.xinitrc
+
+cat >> ~/.bash_profile << BASH_PROFILE
+# Start X on login
+if [[ ! \$DISPLAY && \$XDG_VTNR -eq 1 ]]; then
+    exec startx
+fi
+BASH_PROFILE
+
 bash $CONFIGS/git/__symlink.sh
 bash $CONFIGS/vim/__symlink.sh
 bash $CONFIGS/xfce4/__symlink.sh
@@ -31,14 +62,8 @@ bash $CONFIGS/bash/__symlink.sh
 bash $CONFIGS/i3/__symlink.sh
 bash $CONFIGS/vscode/__symlink.sh
 bash $CONFIGS/compton/__symlink.sh
+bash $CONFIGS/redshift/__symlink.sh
 
-# Install vim
-sudo dnf install vim-enhanced -y
-
-# Generate SSH key
-ssh-keygen -t rsa -b 4096 -C "$(git config --global user.email)"
-
-# Install Fira fonts
 curl -SL https://github.com/mozilla/Fira/archive/4.202.zip -o fira.zip
 unzip fira.zip
 if [ ! $HOME/.fonts ]; then
@@ -47,20 +72,6 @@ fi
 cp Fira-4.202/ttf/*.ttf $HOME/.fonts
 rm -fr Fira-4.202/
 
-# Install Visual Studio Code
 curl -SL https://go.microsoft.com/fwlink/?LinkID=760867 -o vscode.rpm
 sudo dnf install vscode.rpm -y
 rm -f vscode.rpm
-
-# Install Virtualbox
-sudo dnf install VirtualBox -y
-sudo dnf install akmod-VirtualBox kernel-devel-4.8.6-300.fc25.x86_64
-echo "NOTE: Enable kernel mods for VirtualBox"
-#sudo akmods --kernels 4.8.6-300.fc25.x86_64 && systemctl restart systemd-modules-load.service
-
-# Install Vagrant
-sudo dnf install vagrant -y
-
-# Install Gnome tweak tool
-sudo dnf install gnome-tweak-tool
-
