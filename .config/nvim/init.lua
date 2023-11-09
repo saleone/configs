0ce97a29a0a162a0311f8084f1101b7e525ccd49
Saša Savić <sasa@savic.one>
2023-11-09 13:29:17 +0100

    [nvim] Error keybinds

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -166,6 +166,12 @@ vim.keymap.set('v', '>', '>gv')
 -- Do nohlsearch when using escape
 vim.keymap.set('n', '<Esc>', ':nohlsearch<CR><Esc>')
 
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
 ---------------
 --- PLUGINS --|
 ---------------
