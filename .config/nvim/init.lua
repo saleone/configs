4c7c4a6fa6604ddfacca76340e44eaf76b42b207
Saša Savić <sasa@savic.ba>
2025-12-06 22:17:09 +0100

    [nvim] Add DB UI with dadbod

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -151,6 +151,23 @@ require("lazy").setup({
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
     }
+  },
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
   }
 })
 
